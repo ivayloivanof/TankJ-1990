ee197ea287043d6c3d83afac7e36464519f04ee0
ivayloivanof <ivaylo.ivanof@gmail.com>
2015-09-22 09:16:59 +0300

    add jframe on map

--- src/map/Map.java
+++ src/map/Map.java
@@ -1,6 +1,22 @@
 package map;
 
+import javax.swing.*;
+
 public class Map {
     private int width;
     private int hight;
+
+    public Map(int width, int hight) {
+        this.width = width;
+        this.hight = hight;
+        this.createMap();
+    }
+
+    private void createMap() {
+        JFrame frame = new JFrame("TankJ-1990");
+        frame.setVisible(true);
+        frame.setFocusable(true);
+        frame.setSize(this.width, this.hight);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
